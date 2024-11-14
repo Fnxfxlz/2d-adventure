2e4f25f9a2503711ea6690c84cf4ca104f09ab27
Fnxfxlz <ZAriahg4@nycstudents.net>
2024-11-14 09:17:49 -0500

    completed part 10

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,14 @@ public class PlayerController : MonoBehaviour
 {
 
     public InputAction moveAction;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
     // Start is called before the first frame update
     void Start()
     {
 
        moveAction.Enable();
+       rigidbody2d = GetComponent<Rigidbody2D>();
 
        // QualitySettings.vSyncCount = 0;
         // Application.targetFrameRate = 120;
@@ -22,16 +25,20 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
        // Store the moveAction values into a Vector2 variable called move
-       Vector2 move = moveAction.ReadValue<Vector2>();
+       move = moveAction.ReadValue<Vector2>();
 
         // Display the current Vector2 value for move in the Console
         Debug.Log(move);
+       
+    }
 
+
+    void FixedUpdate()
+    {
         // Create a variable called position and add the move value from InputAction to the current
         // Vector2 position
-        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
-        transform.position = position;
+        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
 
-       
     }
 }
